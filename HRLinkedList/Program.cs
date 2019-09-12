807f14412d47079772ffaeaa2ad4708d12b15124
Jim Burnett <jimburnetto@hotmail.com>
2019-09-12 12:45:45 -0500

    begin hrlinkedlist

--- HRLinkedList/Program.cs
+++ HRLinkedList/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
-
-namespace HRLinkedList
+class Node
 {
-    class Program
+    public int data;
+    public Node next;
+    public Node(int d)
     {
-        static void Main(string[] args)
+        data = d;
+        next = null;
+    }
+
+}
+class Solution
+{
+
+    public static Node insert(Node head, int data)
+    {
+        //Complete this method
+    }
+
+    public static void display(Node head)
+    {
+        Node start = head;
+        while (start != null)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write(start.data + " ");
+            start = start.next;
         }
+    }
+    static void Main(String[] args)
+    {
+
+        Node head = null;
+        int T = Int32.Parse(Console.ReadLine());
+        while (T-- > 0)
+        {
+            int data = Int32.Parse(Console.ReadLine());
+            head = insert(head, data);
+        }
+        display(head);
     }
 }
